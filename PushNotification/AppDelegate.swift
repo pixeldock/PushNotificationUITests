8db347fa00dbc469062f517004405f83491093d3
Jörn Schoppe <joern@pixeldock.com>
2017-09-01 15:01:14 +0200

    Ensure that remote notification are registered on the main thread

--- PushNotification/AppDelegate.swift
+++ PushNotification/AppDelegate.swift
@@ -71,7 +71,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UNUserNotificationCenter.current().getNotificationSettings { (settings) in
             guard settings.authorizationStatus == .authorized else { return }
             
-            UIApplication.shared.registerForRemoteNotifications()
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
         }
     }
     
