7c6b987b65d189fd67e7258acc4c64a7eb1cbac7
Jörn Schoppe <joern@pixeldock.com>
2017-09-01 15:01:23 +0200

    Update private header

--- PushNotificationUITests/XCUIApplication.h
+++ PushNotificationUITests/XCUIApplication.h
@@ -34,7 +34,6 @@
 @property(readonly, nonatomic) UIInterfaceOrientation interfaceOrientation; //TODO tvos
 @property(readonly, nonatomic) BOOL running;
 @property(nonatomic) pid_t processID; // @synthesize processID=_processID;
-@property unsigned long long state; // @synthesize state=_state;
 @property(readonly) XCAccessibilityElement *accessibilityElement;
 
 + (instancetype)appWithPID:(pid_t)processID;
